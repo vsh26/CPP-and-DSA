7065f59f4cabe31387c33ce0efcf07608b7b72b0
Vishal Kapgate <vishaldk26@gmail.com>
2024-05-01 14:03:30 +0530

    C++ Basics

---
+++ cppBasics1.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    //DATA TYPES
+        //char, int, float, double, bool
+
+    char c = 'a';
+    int a = 1;
+    double b = 3.5;
+
+    cout << c << endl;  // a
+    cout << a << endl;  // 1
+    cout << b << endl;  // 3.5
+
+    //Putting a double value into int
+    a = 3.5;
+    cout << a << endl;  // 3 (i.e, we can only store int in int)
+
+    bool d = false;
+    bool e = true;
+    bool f = 0;
+    bool g = 1;
+    bool h = 3.2;
+
+    cout << d << endl;  // 0
+    cout << e << endl;  // 1
+    cout << f << endl;  // 0
+    cout << g << endl;  // 1
+    cout << h << endl;  // 1 (every other value apart from 0 is considered as true)
+
+    // ++i (PRE-INCREMENT) -> First increment then use
+    // i++ (POST-INCREMENT) -> First use then increment
+
+    a = 10;
+    cout << a++ << endl;    // 10
+    cout << a << endl;      // 11
+
+    a = 10;
+    cout << ++a << endl;    // 11
+    cout << a << endl;      // 11
+
+    a = 10;
+    b = a++;
+    cout << b << endl;      // 10
+    cout << a << endl;      // 11
+
+    a = 10;
+    b = ++a;
+    cout << b << endl;      // 11
+    cout << a << endl;      // 11
+
+    // Character
+        //Every chatracter has an ASCII value (https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html)
+
+    char m = 'a';
+    char n = 'c';
+
+    cout << m << endl;      // a
+    cout << (int)m << endl; // 97
+    cout << n << endl;      // c
+    cout << (int)n << endl; // 99
+
+    cout << m - n << endl;  // -2
+
+    return 0;
+}
---
+++ cppBasics3.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    // Strings --> Collection of characters
+
+    string str = "Hello";
+    cout << str << endl;        // Hello
+    cout << str.size() << endl; // 5
+
+    string str1;
+    cin >> str1;                // input -> World
+    cout << str1 << endl;       // World
+    cout << str1.size() << endl;// 5
+
+    cin >> str1;                // input -> World ABCD
+    cout << str1 << endl;       // World
+    cout << str1.size() << endl;// 5
+    //(Only "World" is read, as cin stops reading after WHITESPACE or NEWLINE)
+
+    // Concatenation of strings
+    cout << str + " " + str1 << endl;   // Hello World
+
+    // Accessing characters of strings
+    cout << str[0] << endl;     // H
+
+
+    // GETLINE
+    string str2;
+    cin.ignore(); // (Always use after cin to move control to new line)
+    getline(cin, str2);         // input -> World     ABCD
+    cout << str2 << endl;       // World     ABCD
+
+    // push_back
+        // reverse string example
+
+        string str3 = "Hello";
+        string str3_rev;
+
+        // Using loop
+
+            for (int i = str.size() - 1 ; i >= 0 ; --i){
+                str3_rev = str3_rev + str3[i];
+            }
+
+            cout << str3_rev << endl;   // olleH
+
+        string str4 = "Hello";
+        string str4_rev;
+        // Using push_back function
+
+            for (int i = str.size() - 1 ; i >= 0 ; --i ){
+                str4_rev.push_back(str[i]);
+            }
+
+            cout << str4_rev << endl;   // olleH
+    
+    // Always use push_back() function instead of loop method
+        // 1. Avoid adding char in string
+        // 2. Time Complexity
+
+    
+    // Dealing with large numbers
+
+        // Exa: Extracting last digit from string
+        
+        string s = "12345678987654321";
+        int last_digit = s[s.size() - 1];       // (ASCII value of character '1' is extracted)
+        cout << last_digit << endl;             // 49
+        last_digit = s[s.size() - 1] - '0';     // (ASCII value is CONVBERTED into int by subtracting the ASCII value of character '0')
+        cout << last_digit << endl;             // 1
+
+    // IN CP,
+        // For LOCAL ARRAY SIZE <= 10^5, as there is size limit for local arrays, so we have to use GLOBAL ARRAYS
+
+    return 0;
+}
