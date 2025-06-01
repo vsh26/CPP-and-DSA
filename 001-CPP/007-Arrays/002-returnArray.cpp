aa5241b360b64ee1db53c4db4fd9339c31f58b65
Vishal Kapgate <vishaldk26@gmail.com>
2025-06-01 15:56:58 +0530

    Update returnArray.cpp

--- 001-CPP/007-Arrays/002-returnArray.cpp
+++ 001-CPP/007-Arrays/002-returnArray.cpp
@@ -1,15 +1,16 @@
 #include<iostream>
 using namespace std;
 
+//This function that returns a pointer to an integer array
 int* prepareChaiOrders(int numOfCups){
 
-    int* orders = new int[numOfCups];
+    int* orders = new int[numOfCups];       // Dynamically allocate array. Memory allocated in heap
 
     for(int i = 0; i < numOfCups; i++){
-        orders[i] = (i+1) * 10;
+        orders[i] = (i+1) * 10;             // Each cup is (i+1) * 10 ml
     }
 
-    return orders;
+    return orders;                          // Return pointer to the array
 
 }
 
@@ -17,11 +18,13 @@ int main(){
 
     int n = 5;
     
-    int* chaiOrder = prepareChaiOrders(n);
+    int* chaiOrder = prepareChaiOrders(n);  // Get dynamic array from function
 
     for(int i = 0; i < n; i++){
         cout << "Order " << i+1 << ": " << chaiOrder[i] << " ml" << endl;
     }
+
+    delete[] chaiOrder;                     // Free dynamically allocated memory
 
     return 0;
 }
